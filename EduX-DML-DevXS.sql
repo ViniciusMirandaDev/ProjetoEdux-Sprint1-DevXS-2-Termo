2344095bdf8c9a29e553c7b9462b056e730e8b2c
FIC <FIC@info.local>
2020-08-25 17:54:14 -0300

    DML Continuação

--- EduX-DML-DevXS.sql
+++ EduX-DML-DevXS.sql
@@ -2,9 +2,12 @@
 
 USE EduX;
 
+SELECT * FROM Usuario;
+
 INSERT INTO Usuario (Nome,Email,Senha,DataNascimento,Telefone) 
 	
 			VALUES
-			('Vinícius','vinicius.miranda724@gmail.com','dev132','2004-24-06T10:00:00','40028922'),
-			('Paulo','paulo.brandao724@gmail.com','dev132','1994-16-02T09:00:00','93150482');
-		
+			('Vinícius','vinicius.miranda724@gmail.com','dev132','2004-06-24T10:00:00','40028922'),
+			('Paulo','paulo.brandao724@gmail.com','dev132','1994-02-16T09:00:00','93150482');
+	
+INSERT INTO TipoUsuario ()
