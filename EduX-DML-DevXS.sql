19172f925bda66354fb6b728069d04e3f65752d0
Matheus <matheus.barboza.henrique@gmail.com>
2020-08-25 17:25:43 -0300

    DML iniciada

---
+++ EduX-DML-DevXS.sql
@@ -0,0 +1,10 @@
+--DML
+
+USE EduX;
+
+INSERT INTO Usuario (Nome,Email,Senha,DataNascimento,Telefone) 
+	
+			VALUES
+			('Vinícius','vinicius.miranda724@gmail.com','dev132','2004-24-06T10:00:00','40028922'),
+			('Paulo','paulo.brandao724@gmail.com','dev132','1994-16-02T09:00:00','93150482');
+		
